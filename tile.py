17f500f3e99f2805cf3858b1b681d733b614920b
Zims <zims@inbox.lv>
2020-04-04 14:25:54 +0300

    fixed typos

--- tile.py
+++ tile.py
@@ -13,36 +13,31 @@ window.title("How much?")
 
 
 window.geometry('500x500')
-width = Label(window, text="Enter room width:").grid(column=0,row=0)
-length = Label(window, text="Enter room length:").grid(column=0,row=1)
-get_width_label = Entry(window).grid(column=1,row=0)
-get_length_label = Entry(window).grid(column=1,row=1)
-enter_size = Button(window, text="Press to enter").grid(column=1,row=4)
-size_label = Label(window, text="The square footage is:").grid(column=1,row=5)
-size_result = Label(window, text="result of x * y").grid(column=1,row=6)
-size_suggestion = Label(window, text="Industry standard is adding 15% more. That is: ").grid(column=1,row=7)
-size_fixed = Label(window, text="x * y * 1.15").grid(column=1,row=8)
-set_price = Label(window, text="Price of tile:").grid(column=0,row=9)
-tile_price = Entry(window).grid(column=1,row=9)
-complete_cost_label = Label(window, text="The complete cost: ").grid(column=1,row=10)
-complete_cost = Label(window, text="SUMM").grid(column=1,row=11)
+width = Label(window, text="Enter room width:").grid(column=0, row=0)
+length = Label(window, text="Enter room length:").grid(column=0, row=1)
+get_width_label = Entry(window).grid(column=1, row=0)
+get_length_label = Entry(window).grid(column=1, row=1)
+enter_size = Button(window, text="Press to enter").grid(column=1, row=4)
+size_label = Label(window, text="The square footage is:").grid(column=1, row=5)
+size_result = Label(window, text="result of x * y").grid(column=1, row=6)
+size_suggestion = Label(window, text="Industry standard is adding 15% more. That is: ").grid(column=1, row=7)
+size_fixed = Label(window, text="x * y * 1.15").grid(column=1, row=8)
+set_price = Label(window, text="Price of tile:").grid(column=0, row=9)
+tile_price = Entry(window).grid(column=1, row=9)
+complete_cost_label = Label(window, text="The complete cost: ").grid(column=1, row=10)
+complete_cost = Label(window, text="SUM").grid(column=1, row=11)
 
 
-
-
-
-
-# bt = Button(window, text="  Enter  ")
-# bt.pack()
 window.mainloop()
 
 extra_tile = 1.15
 
-def get_lenght():
+
+def get_length():
     while type(input) != int or float:
         try:
-            x_lenght = float(input('*  The floor length in meters? '))
-            return x_lenght
+            x_length = float(input('*  The floor length in meters? '))
+            return x_length
         except ValueError:
             print('Sorry, use a number! (50cm = 0.5)')
 
@@ -59,7 +54,7 @@ def get_width():
 
 
 def get_price():
-    sq_m = get_lenght() * get_width()
+    sq_m = get_length() * get_width()
     price_sqm = None
     while type(price_sqm) != int or float:
         try:
